d433fc2f315db0b8b3580f1925617a358e748954
Eric Chauvin <chauvineric4@gmail.com>
2020-07-30 11:35:52 -0600

    Create locale.h

---
+++ locale.h
@@ -0,0 +1,26 @@
+// -*- C++ -*-
+
+
+
+//===---------------------------- locale.h --------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is dual licensed under the MIT and the University of Illinois Open
+// Source Licenses. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef _LIBCPP_LOCALE_H
+#define _LIBCPP_LOCALE_H
+
+
+#include <__config>
+
+// #if !defined(_LIBCPP_HAS_NO_PRAGMA_SYSTEM_HEADER)
+// #pragma GCC system_header
+// #endif
+
+// #include_next <locale.h>
+
+#endif  // _LIBCPP_LOCALE_H
